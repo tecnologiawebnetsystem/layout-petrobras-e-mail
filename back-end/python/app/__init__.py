3bde23f74415cb6fe095cd25501a6c76c7c80198
Kleber de Oliveira Gonçalves - PrestServ <kleber.goncalves.prestserv+petro@petrobras.com.br>
2026-01-29 20:18:34 -0300

    Atualizacao do back

--- back-end/python/app/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Petrobras File Sharing System - Backend API
-Sistema de compartilhamento seguro de arquivos com aprovação supervisionada
-"""
-
-__version__ = "1.0.0"
--- back-end/python/app/core/security.py
+++
@@ -1,1 +0,0 @@
-# autenticação (JWT / Cognito)
